ffc3ea10f739451769a1612f30e9421f46efa8b6
Peter Toi <peter@petertoi.com>
2018-10-19 09:35:29 -0400

    Refactor deletion. Refactor how actions are parsed on admin save.

--- templates/admin/settings-tab-actions.php
+++ templates/admin/settings-tab-actions.php
@@ -12,72 +12,80 @@
 use USSC_Edgenet\Template;
 
 ?>
-    <h2><?php esc_html_e( 'Actions', 'ussc' ); ?></h2>
-    <table class="form-table">
-    <tbody>
-<?php if ( edgenet()->settings->is_core_valid() ) : ?>
-	<?php
-	echo Template::render_admin_table_row(
-		__( 'Import Requirement Set', 'ussc' ),
-		get_submit_button( __( 'Import Requirement Set', 'ussc' ), 'secondary', 'edgenet_import_requirement_set' )
-	);
-	?>
-<?php endif; ?>
-<?php if ( edgenet()->settings->is_import_valid() ) : ?>
-	<?php
-	echo Template::render_admin_table_row(
-		__( 'Import Products', 'ussc' ),
-		get_submit_button( __( 'Import Products', 'ussc' ), 'secondary', 'edgenet_import_products' )
-	);
+	<h2><?php esc_html_e( 'Actions', 'ussc' ); ?></h2>
+	<table class="form-table">
+		<tbody>
 
-	echo Template::render_admin_table_row(
-		__( 'Import Product By ID', 'ussc' ),
-		sprintf( '%s<br>%s',
-			Template::render_field(
-				'text',
-				'edgenet_import_product_id',
-				'edgenet_import_product_id',
-				'',
-				[ 'class' => 'regular-text' ]
-			),
-			get_submit_button( __( 'Import Product', 'ussc' ), 'secondary', 'edgenet_import_product_by_id' )
-		)
-	);
+		<?php if ( edgenet()->settings->is_core_valid() ) : ?>
 
-	echo Template::render_admin_table_row(
-		__( 'Map Categories', 'ussc' ),
-		get_submit_button( __( 'Map Categories', 'ussc' ), 'secondary', 'edgenet_map_categories' )
-	); ?>
+			<?php
+			echo Template::render_admin_table_row(
+				__( 'Import Requirement Set', 'ussc' ),
+				get_submit_button( __( 'Import Requirement Set', 'ussc' ), 'secondary', 'edgenet_action[import_requirement_set]' )
+			);
+			?>
 
-	<?php if ( edgenet()->debug ) :
+		<?php endif; ?>
+
+		<?php if ( edgenet()->settings->is_import_valid() ) : ?>
+
+			<?php
+			echo Template::render_admin_table_row(
+				__( 'Import Products', 'ussc' ),
+				get_submit_button( __( 'Import Products', 'ussc' ), 'secondary', 'edgenet_action[import_products]' )
+			);
+
+			echo Template::render_admin_table_row(
+				__( 'Import Product By ID', 'ussc' ),
+				sprintf( '%s<br>%s',
+					Template::render_field(
+						'text',
+						'edgenet_import_product_id',
+						'edgenet_import_product_id',
+						'',
+						[ 'class' => 'regular-text' ]
+					),
+					get_submit_button( __( 'Import Product', 'ussc' ), 'secondary', 'edgenet_action[import_product_by_id]' )
+				)
+			);
+
+			echo Template::render_admin_table_row(
+				__( 'Map Categories', 'ussc' ),
+				get_submit_button( __( 'Map Categories', 'ussc' ), 'secondary', 'edgenet_action[map_categories]' )
+			);
+			?>
+
+		<?php endif; ?>
+
+		</tbody>
+	</table>
+
+<?php if ( edgenet()->debug ) : ?>
+	<h2><?php esc_html_e( 'Remove Imported Data', 'ussc' ); ?></h2>
+	<table class="form-table">
+		<tbody>
+		<?php
+		// echo Template::render_admin_table_row(
+		// __( 'Images', 'ussc' ),
+		// get_submit_button( __( 'Delete all', 'ussc' ), 'secondary', 'edgenet_delete_images' ),
+		// [ 'tr' => [ 'class' => 'danger', 'id' => '' ] ]
+		// );
 		echo Template::render_admin_table_row(
-		       '<h2>' . esc_html__( 'Clean data', 'ussc' ). '</h2>', ''
-        );
-
-
-//		echo Template::render_admin_table_row(
-//			__( 'Images', 'ussc' ),
-//			get_submit_button( __( 'Delete all', 'ussc' ), 'secondary', 'edgenet_delete_images' ),
-//            [ 'tr' => [ 'class' => 'danger', 'id' => '' ] ]
-//		);
-		echo Template::render_admin_table_row(
-			__( 'Products', 'ussc' ),
-			get_submit_button( __( 'Delete all', 'ussc' ), 'secondary', 'edgenet_delete_product' ),
+			__( 'Delete Products', 'ussc' ),
+			get_submit_button( __( 'Delete Products', 'ussc' ), 'alert', 'edgenet_action[delete_products]' ),
 			[ 'tr' => [ 'class' => 'danger', 'id' => '' ] ]
 		);
 		echo Template::render_admin_table_row(
-			__( 'Documents', 'ussc' ),
-			get_submit_button( __( 'Delete all', 'ussc' ), 'secondary', 'edgenet_delete_docs' ),
+			__( 'Delete Documents', 'ussc' ),
+			get_submit_button( __( 'Delete Documents', 'ussc' ), 'alert', 'edgenet_action[delete_documents]' ),
 			[ 'tr' => [ 'class' => 'danger', 'id' => '' ] ]
 		);
 		echo Template::render_admin_table_row(
-			__( 'Reset', 'ussc' ),
-			get_submit_button( __( 'Delete all', 'ussc' ), 'secondary', 'edgenet_delete_all' ),
+			__( 'Delete All', 'ussc' ),
+			get_submit_button( __( 'Delete All', 'ussc' ), 'alert', 'edgenet_action[delete_all]' ),
 			[ 'tr' => [ 'class' => 'danger', 'id' => '' ] ]
 		);
 		?>
-	<?php endif; ?>
-    <?php endif; ?>
-	</tbody>
-</table>
-
+		</tbody>
+	</table>
+<?php endif;
